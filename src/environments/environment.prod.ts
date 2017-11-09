d46293565bb9b71422bc90c7441b836ac84d5f37
cecy <cecilia1995andrade@gmail.com>
2017-11-09 15:29:41 -0600

    Githup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyAntq6Npikl0Zk1uh6Bj4OnHI573xP2GEc",
+    authDomain: "registroempleados-124cf.firebaseapp.com",
+    databaseURL: "https://registroempleados-124cf.firebaseio.com",
+    projectId: "registroempleados-124cf",
+    storageBucket: "registroempleados-124cf.appspot.com",
+    messagingSenderId: "630822624695"
+  }
 };
