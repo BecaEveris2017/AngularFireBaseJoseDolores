9c1913fb2f30b7543c6cf1ee01900915ac0bd667
cecy <cecilia1995andrade@gmail.com>
2017-11-09 15:12:46 -0600

    angular confirebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyAntq6Npikl0Zk1uh6Bj4OnHI573xP2GEc",
+    authDomain: "registroempleados-124cf.firebaseapp.com",
+    databaseURL: "https://registroempleados-124cf.firebaseio.com",
+    projectId: "registroempleados-124cf",
+    storageBucket: "registroempleados-124cf.appspot.com",
+    messagingSenderId: "630822624695"
+  }
 };
